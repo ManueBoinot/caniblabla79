a95385b5cb41b4acb30cf83832b06080a40d257d
ManueBoinot <116074499+ManueBoinot@users.noreply.github.com>
2022-12-05 15:49:56 +0100

    creattion du depot

---
+++ database/migrations/2022_12_05_135421_create_message_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            // génère un ID auto-increment unique et no null
+            $table->id();
+            // contenu du message max 3000 caractères
+            $table->text("contenu", 3000);
+            // clé étrangère vers table USERS
+            $table->foreignId("user_id")->constrained();
+            // image associée message
+            $table->string("image");
+            // tags de l'image
+            $table->string("tags", 50);
+            // champs created_at et updated_at
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+};
